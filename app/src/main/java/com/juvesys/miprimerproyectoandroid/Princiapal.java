862f3b7d352fd3f729b42bf6f00ce48b9bd82a39
juvesys <juvesys@gmail.com>
2016-04-18 01:32:05 -0500

    Initial commit

---
+++ app/src/main/java/com/juvesys/miprimerproyectoandroid/Princiapal.java
@@ -0,0 +1,13 @@
+package com.juvesys.miprimerproyectoandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Princiapal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_princiapal);
+    }
+}
